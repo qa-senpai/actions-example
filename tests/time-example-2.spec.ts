e584ddcb5ce85312484322006b09adbb5f382f66
Pavlo <safonovcoach@gmail.com>
2025-12-17 20:03:29 +0200

    Add Playwright testing framework and initial test cases

---
+++ tests/time-example-2.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("test second", () => {
+  test("60s - 2", async ({ page }) => {
+    await page.waitForTimeout(20_000);
+    await page.locator("#click").click({ timeout: 1000 });
+  });
+});
+
+test("10s - 2", async ({ page }) => {
+  await page.waitForTimeout(10_000);
+});
+
+test("30s - 2", async ({ page }) => {
+  await page.waitForTimeout(30_000);
+});
+
+test("40s - 2", async ({ page }) => {
+  await page.waitForTimeout(40_000);
+});
+
+test("120s - 2", async ({ page }) => {
+  await page.waitForTimeout(120_000);
+});
+
+test("1s - 2", async ({ page }) => {
+  await page.waitForTimeout(1_000);
+});
